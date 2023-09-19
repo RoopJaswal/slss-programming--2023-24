1c524991c86de79b2225fdcae117ca254e7d2fa9
Roop Jaswal <Roopj304@gmail.com>
2023-09-19 10:55:05 -0700

    Added some new notes and Python files

---
+++ Notes/quotegenerator.py
@@ -0,0 +1,18 @@
+# Quote Generator 
+# Author: Ubial 
+# Date: 19 September 2023 
+
+# Output a quote from someone famouse 
+print ("Do or do not. There is no try. --Yoda")
+
+# Print out another quote 
+print ("Trust the process. --Joel Embiid ")
+
+# Print out another, different quote 
+print ("Job's not finished. --Kobe Bryant")
+
+# Ask the user a question 
+input("What was you favourite quote?")
+
+# Respond with something a human would say 
+print ("Your quote inspired me")
